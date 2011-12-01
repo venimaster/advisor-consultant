87ba8c2a3f77df4da2afe14dacc5036be4d4c20b
daianovich@gmail.com <daianovich@gmail.com@71ee1ffb-5df3-2541-44d3-b0d068acd847>
2011-12-01 09:29:50 +0000

    новые методы

--- SOZ/dbwork.cpp
+++ SOZ/dbwork.cpp
@@ -63,6 +63,12 @@ dbWork::dbWork()
 
     //выводит варианты ответа на вопрос из ideal_expert с номером id
     qDebug()<<GetAnswerVariants("ideal_expert",id);
+
+
+    qDebug()<<GetAttribute("user","u_id","login",1);
+    qDebug()<<GetAttribute("answer_variant_list","a_v_id","answer_variant_1",1);
+    qDebug()<<GetAttribute("system","id","system_name",2);
+
 //ВВОД
     // вводит в поле o_r_result значение 1, 0 - невозможно применять технологию СОЗ 1 - возможно
     SetResult("venima", "o_r_result",1);
@@ -71,10 +77,10 @@ dbWork::dbWork()
     // вводит в таблицу s_result данному юзеру значение 1 - т.е. ему было решено посоветовать систему с id 1
     SetResult("venima", "s_result",1);
 
-    //SetRegistrationInfo("prep", "123","Рыбина Галина Валентиновна", "ПК СОЗ");
+    SetRegistrationInfo("prep", "123","Рыбина Галина Валентиновна", "ПК СОЗ");
     SetRegistrationInfo("stud", "123","Кусков Иван Михайлович", "ПК СОЗ","эксперт","муж",21);
-
-
+    // создает пару из 2 и 3.
+    SetPair (2,3);
 }
 
 bool dbWork::connectDB(QString _dbType, QString _dbName, QString _host, QString _dbUser, QString _dbPass)
@@ -172,7 +178,8 @@ void dbWork::SetResult(QString login, QString columnName,int result)
         query.exec("INSERT INTO `db`.`k_r_result`(`id`, `u_id`) VALUES ('"+QString::number(result)+"','"+QString::number(u_id)+"')");
     break;
     case 2:
-        query.exec("UPDATE `db`.`user` SET `"+columnName+"` = '"+QString::number(result)+"' WHERE `user`.`login` ='"+login+"';");
+            query.exec("UPDATE `db`.`user` SET `"+columnName+"` = '"+QString::number(result)+"' WHERE `user`.`login` ='"+login+"';");
+    break;
     }
 }
 void dbWork::SetResult(QString login, QString columnName,QString result){
@@ -197,14 +204,10 @@ void dbWork::SetRegistrationInfo(QString login, QString password,QString name, Q
      query.exec("SELECT `u_id` FROM `user` WHERE `user`.`login`='"+login+"'");
      query.next();
      u_id=query.value(0).toInt();
-     qDebug()<<u_id;
-
      //получаем pr_id
      query.exec("SELECT `id` FROM `project` WHERE `project`.`project_name`='"+projectName+"'");
      query.next();
      pr_id=query.value(0).toInt();
-     qDebug()<<pr_id;
-
      //вносим информацию о принадлежности пользователя проекту
      query.exec("INSERT INTO `project_group` (`u_id` ,`pr_id`)"
                 "VALUES ('"+QString::number(u_id)+"','"+QString::number(pr_id)+"')");
@@ -220,9 +223,6 @@ void dbWork::SetRegistrationInfo(QString login, QString password,QString name, Q
     query.exec("SELECT `id` FROM `user_group` WHERE `user_group`.`group_name`='"+groupName+"'");
     query.next();
     u_g_id=query.value(0).toInt();
-    qDebug()<<u_g_id;
-
-
     //заносим регистрационные данные в user (добавление пользователя)
     query.exec("INSERT INTO `user` (`u_id` ,`password` ,`name` ,`gender` ,`age` ,`u_g_id` ,`login` ,`competience_result` ,`r_r_result` ,`p_r_result` ,`o_r_result` ,`research_result` ,`f_f_result` ,`c_s_result` ,`r_v_result` ,`t_t_result` ,`prototyping_strategy_result`)"
                "VALUES('','"+password+"','"+name+"','"+gender+"','"+QString::number(age)+"','"+QString::number(u_g_id)+"','"+login+"','','','','','','','','','','')");
@@ -231,14 +231,10 @@ void dbWork::SetRegistrationInfo(QString login, QString password,QString name, Q
     query.exec("SELECT `u_id` FROM `user` WHERE `user`.`login`='"+login+"'");
     query.next();
     u_id=query.value(0).toInt();
-    qDebug()<<u_id;
-
     //получаем pr_id
     query.exec("SELECT `id` FROM `project` WHERE `project`.`project_name`='"+projectName+"'");
     query.next();
     pr_id=query.value(0).toInt();
-    qDebug()<<pr_id;
-
     //вносим информацию о принадлежности пользователя проекту
     query.exec("INSERT INTO `project_group` (`u_id` ,`pr_id`)"
                "VALUES ('"+QString::number(u_id)+"','"+QString::number(pr_id)+"')");
@@ -246,6 +242,50 @@ void dbWork::SetRegistrationInfo(QString login, QString password,QString name, Q
 }
 
 
+QVector<QString> dbWork::GetAttribute(QString tableName, QString idColumnName, QString attributeColumnName, int id)
+{
+    QVector<QString> attributeList;
+    QSqlQuery query;
+    query.exec("SELECT `"+attributeColumnName+"` FROM `"+tableName+"`WHERE `"+idColumnName+"`='"+QString::number(id)+"';");
+    while(query.next())
+    {
+        attributeList<<query.value(0).toString();
+    }
+    return attributeList;
+}
+
+QVector<QString> dbWork::GetAttribute(QString tableName, QString idColumnName, QString attributeColumnName, QString id)
+{
+    QVector<QString> attributeList;
+    QSqlQuery query;
+    query.exec("SELECT `"+attributeColumnName+"` FROM `"+tableName+"`WHERE `"+idColumnName+"`='"+id+"';");
+    while(query.next())
+    {
+        attributeList<<query.value(0).toString();
+    }
+    return attributeList;
+}
+
+QVector<int> dbWork::GetProjectGroup(QString projectName)
+{
+    QVector<int> groupList;
+    QSqlQuery query;
+    QString pr_id;
+    // получаем идентификатор группы
+//    pr_id <<  GetAttribute("project","project_name","id",projectName);
+
+//    query.exec("SELECT `u_id` FROM `project_group` WHERE `pr_id`='"+pr_id+"' AND ");
+
+
+    return groupList;
+}
+
+void dbWork::SetPair (int u_id, int pair_id)
+{
+    QSqlQuery query;
+    query.exec("INSERT INTO `ideal_pair` (`u_id` ,`pair_id`)"
+               "VALUES ('"+QString::number(u_id)+"','"+QString::number(pair_id)+"')");
+}
 
 dbWork::~dbWork()
 {
